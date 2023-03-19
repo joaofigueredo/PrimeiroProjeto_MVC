98ba447ded97a1d434b2bc9fd985247192afb9d9
Joao Carlos <joaofigueredo6@gmail.com>
2023-03-19 11:40:09 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto_MVC/Controllers/TesteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeiroProjeto_MVC.Controllers
+{
+    public class TesteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Demo()
+        {
+            return View();
+        }
+    }
+}
