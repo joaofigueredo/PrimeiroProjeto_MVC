86f360a3a53b10d4ce3e10d62e306e874a33493d
Joao Carlos <joaofigueredo6@gmail.com>
2023-03-20 20:50:32 -0300

    realizando migration e data annotations

--- PrimeiroProjeto_MVC/Models/Categoria.cs
+++ PrimeiroProjeto_MVC/Models/Categoria.cs
@@ -1,9 +1,20 @@
-﻿namespace PrimeiroProjeto_MVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PrimeiroProjeto_MVC.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+        [StringLength(100, ErrorMessage ="Tamanho maximo é 100 caracteres")]
+        [Required(ErrorMessage ="Informe o nome da Categoria")]
+        [Display (Name ="Nome")]
         public string  CategoriaNome { get; set; }
+        [StringLength(200, ErrorMessage = "Tamanho maximo é 200 caracteres")]
+        [Required(ErrorMessage = "Informe a descrição da categoria")]
+        [Display(Name = "Descrição")]
         public int Descricao { get; set; }
 
         public List<Lanche> Lanches { get; set; }
--- PrimeiroProjeto_MVC/Models/Lanche.cs
+++ PrimeiroProjeto_MVC/Models/Lanche.cs
@@ -1,15 +1,46 @@
-﻿namespace PrimeiroProjeto_MVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PrimeiroProjeto_MVC.Models
 {
+    [Table("Lanches")]
     public class Lanche
     {
+        [Key]
         public int LancheId { get; set; }
+
+        [Required(ErrorMessage ="Informe o nome do lanche!")]
+        [StringLength(100,MinimumLength = 10, ErrorMessage ="O Maximo de caracteres é 100 e o minimo é 10")]
+        [Display(Name ="Nome do Lanche")]
         public string NomeLanche{ get; set; }
+
+        [Required(ErrorMessage = "Informe o nome do lanche!")]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "O Maximo de caracteres é 100 e o minimo é 10")]
+        [Display(Name = "descrição curta do Lanche")]
         public string DescricaoCurta { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome do lanche!")]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "O Maximo de caracteres é 100 e o minimo é 10")]
+        [Display(Name = "Descrição detalhada do Lanche")]
         public string DescricaoDetalhada { get; set; }
+
+        [Required(ErrorMessage ="Informe o preço do lanche")]
+        [Display(Name = "Preço")]
+        [Column(TypeName ="decimal(10,2)")]
+        [Range(1, 999.99, ErrorMessage ="O preço deve estar entre 1 e 999,99")]
         public decimal Preco { get; set; }
+
+        [Display(Name = "Caminho Imagem Normal")]
+        [StringLength(200, ErrorMessage = "O Maximo de caracteres é 200")]
         public string ImagemUrl{ get; set; }
+
+        [Display(Name = "Caminho Imagem pequena")]
+        [StringLength(200, ErrorMessage = "O Maximo de caracteres é 200")]
         public string ImagemThumbnailUrl{ get; set; }
+
+        [Display(Name="Lanche Preferido")]
         public bool IsLanchePreferido{ get; set; }
+        [Display(Name ="Estoque")]
         public bool EmEstoque{ get; set; }
 
         public int CategoriaId { get; set; }
