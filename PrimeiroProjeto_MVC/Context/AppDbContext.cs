669e4cdeec44a70ff99c120547f723b3cd68fb66
Joao Carlos <joaofigueredo6@gmail.com>
2023-04-10 19:45:29 -0300

    Busca de alimentos por nomes.

--- PrimeiroProjeto_MVC/Context/AppDbContext.cs
+++ PrimeiroProjeto_MVC/Context/AppDbContext.cs
@@ -12,6 +12,8 @@ namespace PrimeiroProjeto_MVC.Context
         //mapeia a classe, primeiro nome, para a tabela, segundo nome
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Lanche> Lanches { get; set; }
-        public DbSet<CarrinhoCompraItem> CarrinhoCompraItens{ get; set; }
+        public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoDetalhe> PedidoDetalhes { get; set; }
     }
 }
---
+++ PrimeiroProjeto_MVC/Repositories/Interfaces/IPedidoRepository.cs
@@ -0,0 +1,9 @@
+﻿using PrimeiroProjeto_MVC.Models;
+
+namespace PrimeiroProjeto_MVC.Repositories.Interfaces
+{
+    public interface IPedidoRepository
+    {
+        void CriarPedido(Pedido pedido);
+    }
+}
